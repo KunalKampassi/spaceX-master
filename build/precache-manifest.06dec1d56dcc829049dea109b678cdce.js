25b2f3ce152c4f2cb972109422c51283eb546649
KunalKampassi <kunalkampassi@gmail.com>
2021-01-29 22:56:18 +0530

    new changes

---
+++ build/precache-manifest.06dec1d56dcc829049dea109b678cdce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97eeec7cb918d37774ec68cd4a5902c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9573615cff53fe410a04",
+    "url": "/static/css/main.8e6695a8.chunk.css"
+  },
+  {
+    "revision": "ebbed7f3bffea465b8c6",
+    "url": "/static/js/2.0ed44994.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0ed44994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9573615cff53fe410a04",
+    "url": "/static/js/main.540651d8.chunk.js"
+  },
+  {
+    "revision": "ddc36a53c4c1d2135c1d",
+    "url": "/static/js/runtime-main.91e77160.js"
+  }
+]);
